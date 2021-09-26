03a371b9c9b241feffb29e33d92192b4867adec6
miura0617 <j.miura0617@gmail.com>
2021-09-27 01:39:17 +0900

    section3 code

---
+++ backend/tests/test_routes/test_jobs.py
@@ -0,0 +1,37 @@
+import json
+
+
+# clientは、conftest.pyで定義しているものが使える
+def test_create_job(client):
+    # ジョブ作成
+    data = {
+        "title": "title123",
+        "company": "company123",
+        "company_url": "https://www.fdj.com",
+        "location": "Japan",
+        "description": "This is emergency!!!",
+        "date_posted": "2022-07-28"
+    }
+    response = client.post("/jobs/create-job", json.dumps(data))
+    assert response.status_code == 200
+    assert response.json()["title"] == "title123"
+    assert response.json()["company"] == "company123"
+    assert response.json()["location"] == "Japan"
+    assert response.json()["description"] == "This is emergency!!!"
+
+
+def test_retreive_job_by_id(client):
+    # ジョブ作成
+    data = {
+        "title": "title123",
+        "company": "company123",
+        "company_url": "https://www.fdj.com",
+        "location": "Japan",
+        "description": "This is emergency!!!",
+        "date_posted": "2022-07-28"
+    }
+    client.post("/jobs/create-job", json.dumps(data))
+
+    response = client.get("/jobs/get/1")
+    assert response.status_code == 200
+    assert response.json()["title"] == "title123"
