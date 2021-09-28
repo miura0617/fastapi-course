825db4ebebb909d80ee592ad33cc28146d802cc5
miura0617 <j.miura0617@gmail.com>
2021-09-29 00:43:01 +0900

    ListView of Jobsを作成

--- backend/webapps/jobs/route_jobs.py
+++ backend/webapps/jobs/route_jobs.py
@@ -1,7 +1,12 @@
 from fastapi import APIRouter, requests
 from fastapi import Request
+from fastapi.params import Depends
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm.session import Session
+from db.repository.jobs import list_jobs
+from sqlalchemy.orm import Session
+from db.session import get_db
 
 
 template = Jinja2Templates(directory="templates")
@@ -11,6 +16,7 @@ router = APIRouter(include_in_schema=False)
 
 
 @router.get("/")
-def home(request:Request):
-    print(dir(requests))
-    return template.TemplateResponse("jobs/homepage.html", {"request":request})
+def home(request:Request, db:Session=Depends(get_db)):
+    jobs = list_jobs(db=db)
+    # print(dir(requests))
+    return template.TemplateResponse("jobs/homepage.html", {"request":request, "jobs":jobs})
