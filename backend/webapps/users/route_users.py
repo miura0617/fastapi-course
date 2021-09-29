b459806208826c1ca4dd3094f467bd424e63178b
miura0617 <j.miura0617@gmail.com>
2021-09-29 14:30:17 +0900

    user registration(signup)のgetメソッド実装

---
+++ backend/webapps/users/route_users.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+from fastapi.exceptions import FastAPIError
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+
+
+router = APIRouter(include_in_schema=False)
+templates = Jinja2Templates(directory="templates")
+
+
+@router.get("/register/")
+def register(request: Request):
+    return templates.TemplateResponse("users/register.html", {"request": request})
